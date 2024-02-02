2f90db695b7b6a382df4f30c0991dfc03a038470
koos.van.strien <koos.van.strien@wortellsmartlearning.onmicrosoft.com>
2024-02-02 13:58:46 +0000

    Basic Manipulations uitgewerkt in Spark

--- 02_Exercise_Basic_Manipulations.Notebook/notebook-content.py
+++ 02_Exercise_Basic_Manipulations.Notebook/notebook-content.py
@@ -23,60 +23,84 @@
 
 # MARKDOWN ********************
 
-#  1) import pandas and read in the movie dataset (`'https://github.com/wortell-smart-learning/python-data-fundamentals/raw/main/data/most_voted_titles_enriched.csv'`) and assign to a variable df 
+# 1. Acquire a Spark session, read in the movie dataset (`'Files/csvsources/most_voted_titles_enriched.csv'`) and assign to a variable df
+# 
+# Besides setting the header and schema, add `multiLine=True` option to `spark.read.csv()`
 
 # CELL ********************
 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('02_exercise').getOrCreate()
+
+# CELL ********************
+
+df = spark.read.csv('Files/csvsources/most_voted_titles_enriched.csv', header=True, inferSchema=True, multiLine=True)
 
 # MARKDOWN ********************
 
-#  2) Run the following line to make sure you can see all the columns of your dataframe
+# 2. Check the first few lines of your dataframe
 
 # CELL ********************
 
-pd.options.display.max_columns = 100
+display(
+    df.limit(3)
+)
 
 # MARKDOWN ********************
 
-#  3) Check the first few lines of your dataframe
+# Get the summary statistics of your dataframe using `.describe()`. 
+# 
+# In order for `describe` to work, you need to select only the numeric columns ('isAdult', 'startYear', 'endYear', 'runtimeMinutes', 'averageRating', 'numVotes', 'metascore' - but if all is too easy, select the numeric columns programmatically 🙂)
+# 
+# 3. What is the mean averageRating and what is the max averageRating for the movies?
 
 # CELL ********************
 
+numeric_types = ["int", "double", "float", "decimal"]
+numeric_cols = [field[0] for field in df.dtypes if field[1] in numeric_types]
+print(numeric_cols)
+
+# CELL ********************
+
+display(
+    df.select(numeric_cols).describe()
+)
 
 # MARKDOWN ********************
 
-#  4) Get the summary statistics of your dataframe using `.describe()`. What is the mean averageRating and what is the max averageRating for the movies?
+# 5. Sort your dataframe on `runtimeMinutes`. What is the shortest and longest (use argument `ascending=False`) movie or tv series in this dataset?
 
 # CELL ********************
 
+display(
+    df.sort('runtimeMinutes').limit(1)
+)
+
+# CELL ********************
+
+display(
+    df.sort('runtimeMinutes', ascending=False).limit(1)
+)
 
 # MARKDOWN ********************
 
-#  5) Sort your dataframe on `runtimeMinutes`. What is the shortest and longest (use argument `ascending=False`) movie or tv series in this dataset?
+# 6. In the answer to the question before we almost only saw tv-series. Sort on both `titleType` and `runtimeMinutes` to figure out what the longest movie is in this dataset.
 
 # CELL ********************
 
+display(
+    df.sort(['titleType', 'runtimeMinutes'], ascending=[True, False]).limit(1)
+)
 
 # MARKDOWN ********************
 
-#  6) In the answer to the question before we almost only saw tv-series. Sort on both `titleType` and `runtimeMinutes` to figure out what the longest movie is in this dataset.
+# 7. We are going to delete column `endYear`. Don't forget to assign it back to your variable df. Also check make sure that your df now has 25 columns instead of 26 columns.
 
 # CELL ********************
 
-
-# MARKDOWN ********************
-
-#  7) Check which column has the most null values.
-
-# CELL ********************
-
-
-# MARKDOWN ********************
-
-#  8) We are going to delete column `endYear`. Don't forget to assign it back to your variable df. Also check make sure that your df now has 25 columns instead of 26 columns.
-
-# CELL ********************
-
+df = df.drop('endYear')
+len(df.columns)
 
 # MARKDOWN ********************
 
@@ -84,10 +108,20 @@ pd.options.display.max_columns = 100
 
 # CELL ********************
 
+df = df.withColumn("new_rating", df.averageRating * 10)
+display(
+    df.select("primaryTitle", "averageRating", "new_rating")
+)
 
 # MARKDOWN ********************
 
-#  10) Do a value counts to find out how many adult movies are in this dataset (use column `isAdult`)
+#  10) Find out how many adult movies are in this dataset (use column `isAdult`)
+
+# CELL ********************
+
+display(
+    df.groupBy('isAdult').count()
+)
 
 # CELL ********************
 
